be2d4377f7399837a3b789a59cddaa8e5a5dad13
JonasGeschke <jonas.geschke@ips.unibe.ch>
2021-10-14 14:05:18 +0200

    add broad web access

--- R/gmba_read.R
+++ R/gmba_read.R
@@ -3,9 +3,11 @@
 #' Read the GMBA Inventory v2.0 (standard extend, all layers) to R, from the web or the local drive. This function is to be used
 #' without an assignment operator (e.g. "<-").
 #'
-#' @param source Character string, options are \emph{web} (Default) or \emph{local}
+#' @param source Character string, options are \emph{"web"} (Default) or \emph{"local"}
+#' @param extend Character string, options are \emph{"standard"} (Default) or \emph{"broad"}. Check \url{https://www.earthenv.org/mountains}
+#' for the details on the options. Ignored when \code{source} is set to \emph{"local"}
 #' @param local Character string containing the path to the inventory shapefile on the local drive.
-#' Only needed when \code{source} is set to \emph{local}. Default is set to \emph{NULL}
+#' Only needed when \code{source} is set to \emph{"local"}. Default is set to \emph{NULL}
 #'
 #' @return \code{gmba_inv()}, a function containing the GMBA Inventory v2.0
 #'
@@ -18,10 +20,11 @@
 #' @examples
 #' \dontrun{
 #' gmba_read("web")
-#' gmba_read(source = "local", local = "GMBA_Inventory_V2_All_Attributes.shp")
+#' gmba_read("web", extend = "broad")
+#' gmba_read(source = "local", local = "GMBA_Inventory_v2.0.shp")
 #' }
 
-gmba_read <- function(source = "web", local = NULL) { # WEB TO ADD
+gmba_read <- function(source = "web", extend = "standard", local = NULL) {
 
   ##### check attributes
   source <- match.arg(source, c("web", "local"))
@@ -29,8 +32,14 @@ gmba_read <- function(source = "web", local = NULL) { # WEB TO ADD
   ##### run function
   # source = web
   if(source == "web"){
-    # set the inventory web link (default layer: standard extent, all layers)
-    webfile <- "https://data.earthenv.org/mountains/standard/GMBA_Inventory_v2.0.zip"
+    extend <- match.arg(extend, c("standard", "broad"))
+    # set the inventory web link
+    if(extend == "standard"){ # default layer: standard extent, all layers
+      webfile <- "https://data.earthenv.org/mountains/standard/GMBA_Inventory_v2.0.zip"
+    }
+    if(extend == "broad"){
+      webfile <- "https://data.earthenv.org/mountains/broad/GMBA_Inventory_v2.0_broad.zip"
+    }
     # create temp files
     temp <- tempfile()
     temp2 <- tempfile()
@@ -42,7 +51,12 @@ gmba_read <- function(source = "web", local = NULL) { # WEB TO ADD
     # the $ at the end of ".shp$" ensures we are not also finding files such as .shp.xml
     shp_file <- list.files(temp2, pattern = ".shp$", full.names=TRUE)
     # read the shapefile
-    gmba_inventory_v_2_0 <- st_read(shp_file, quiet=TRUE)
+    if(extend == "standard"){
+      gmba_inventory_v_2_0 <- st_read(shp_file, quiet=TRUE)
+    }
+    if(extend == "broad"){
+      gmba_inventory_v_2_0_broad <- st_read(shp_file, quiet=TRUE)
+    }
   }
 
   # source = local
@@ -55,11 +69,25 @@ gmba_read <- function(source = "web", local = NULL) { # WEB TO ADD
   }
 
   ##### return the inventory
-  gmba_inventory_v_2_0$GMBA_V2_ID <- as.character(gmba_inventory_v_2_0$GMBA_V2_ID)
-  return(assign("gmba_inv",
-                local({
-                  function() gmba_inventory_v_2_0
-                }),
-                envir = parent.frame()))
+  # standard extend
+  if(exists("gmba_inventory_v_2_0")){
+    gmba_inventory_v_2_0$GMBA_V2_ID <- as.character(gmba_inventory_v_2_0$GMBA_V2_ID)
+    return(assign("gmba_inv",
+                  local({
+                    function() gmba_inventory_v_2_0
+                  }),
+                  envir = parent.frame()))
+  }
+  # broad extend
+  if(exists("gmba_inventory_v_2_0_broad")){
+    warning("Note you read the GMBA Inventory 2.0 in its broad extend. With this, some of the gmbaR functions might not fully work, as they depend on attributes only available in the standard extend version.",
+            immediate. = TRUE)
+    gmba_inventory_v_2_0_broad$GMBA_V2_ID <- as.character(gmba_inventory_v_2_0_broad$GMBA_V2_ID)
+    return(assign("gmba_inv",
+                  local({
+                    function() gmba_inventory_v_2_0_broad
+                  }),
+                  envir = parent.frame()))
+  }
 
 }
